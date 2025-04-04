81b4fec6b2e5467ef0000dcc48f0c2f968b7a3ae
ocean <twyeo9006@gmail.com>
2025-04-04 15:52:16 +0900

    plugin : 테일윈드 인텔리전스 세팅추가

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const TodoList = () => {
-  return <div class="bg-red-50">TodoList</div>;
+  return <div className="bg-red-50">TodoList</div>;
 };
 
 export default TodoList;
